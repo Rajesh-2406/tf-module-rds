e55f3402c58d960ee6d8a444233d104debfe8ae6
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-29 16:28:02 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -21,29 +21,29 @@ resource "aws_security_group" "sg" {
   }
 }
 resource "aws_db_subnet_group" "main" {
-  name = "main"
+  name       = "main"
   subnet_ids = var.subnet_ids
-  tags =  merge({ Name = "${var.component}-${var.env}"}, var.tags)
+  tags       =  merge({ Name = "${var.component}-${var.env}"}, var.tags)
 }
 
 resource "aws_rds_cluster" "main" {
-  cluster_identifier =  "${var.component}-${var.env}"
-  engine = var.engine
-  engine_version = var.engine_version
-  database_name = var.database_name
-  master_username = data.aws_ssm_parameter.username.value
-  master_password = data.aws_ssm_parameter.password.value
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  storage_encrypted = true
-  kms_key_id = var.kms_key_arn
+  cluster_identifier     =  "${var.component}-${var.env}"
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  database_name          = var.database_name
+  master_username        =  data.aws_ssm_parameter.username.value
+  master_password        = data.aws_ssm_parameter.password.value
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  storage_encrypted      = true
+  kms_key_id             = var.kms_key_arn
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
 resource "aws_rds_cluster_instance" "main" {
-  count = var.instance_count
-  identifier = "${var.component}-${var.env}-instance-${count.index}"
+  count              = var.instance_count
+  identifier         = "${var.component}-${var.env}-instance-${count.index}"
   cluster_identifier = aws_rds_cluster.main.id
-  instance_class = var.instance_class
-  engine = var.engine
-  engine_version = var.engine_version
+  instance_class     = var.instance_class
+  engine             = var.engine
+  engine_version     = var.engine_version
 }
