10843d5748e96141b02e8e90250e3db92261f076
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-03 12:19:06 +0530

    terraform project

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_rds_cluster" "main" {
   database_name          = var.db_name
   kms_key_id             = var.kms_key_arn
   vpc_security_group_ids = [aws_security_group.sg.id]
-  skip_final_snapshot    = false
+  skip_final_snapshot    = true
 
 }
 
