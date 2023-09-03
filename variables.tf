be27b14b91231eff762e42e5eb14e5dff4704778
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-03 11:02:22 +0530

    terraform project

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "tags" {}
 variable "kms_key_arn" {}
-variable "instance_count" {}
+variable "db_instance_count" {}
 variable "sg_subnet_cidr" {}
 variable "vpc_id" {}
 variable "instance_class" {}
